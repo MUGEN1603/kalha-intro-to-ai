be8944608792ce204af2db25c11c25a4b4caf16b
Jesper Aaløse <Jesper.c.aalose@live.dk>
2026-03-19 12:58:14 +0100

    Added comments to state

--- game/state.py
+++ game/state.py
@@ -17,6 +17,8 @@ def initial_state() -> KalahaState:
     """Returns the standard starting position for Kalaha."""
     return KalahaState(
         pits=((4, 4, 4, 4, 4, 4), (4, 4, 4, 4, 4, 4)),
+        #Explained: pits=(("P1", "P2", "P3", "P4", "P5", "P6"), ("P7", "P8", "P9", "P10", "P11", "P12")),
         stores=(0, 0),
+        #Explained: Stores=("S1", "S2")
         current_player=0
     )
